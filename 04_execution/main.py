5f9b06dc07b9dddb2005dce0d802012e5fd99637
Sebastian-Arroyo <sebastian.arroyo1@udea.edu.co>
2024-11-26 13:05:23 -0500

    Created execution file

---
+++ 04_execution/main.py
@@ -0,0 +1,15 @@
+'''
+Proyecto 1 de PDI
+Autores:
+    Jorge Sebastian Arroyo Estrada     sebastian.arroyo1@udea.edu.co
+    CC:1193482707
+    Daniel Felipe Yépez Taimal         daniel.yepez@udea.edu.co
+    CC:1004193180
+
+PREPROCESADO DE BASE DE DATOS
+1. Quitar imágenes con fondos que no sean blancos
+2. Ampliar base de datos (translaciones y rotaciones)
+3. Ampliar base de datos (imágenes con ruido de sal y pimienta)
+4. Quitar imágenes que sean muy pequeñas o muy grandes ()
+'''
+
